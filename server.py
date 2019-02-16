832af08a5d1afe063043413f2e63f6a3c77935f8
Sol1323 <solimar.bp90@gmail.com>
2019-02-16 15:56:44 -0800

    changed NaturalDisaster name for import in server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import User, Contact, Phone, Alert, Natural_Disaster, Earthquake, connect_to_db, db
+from model import User, Contact, Phone, Alert, NaturalDisaster, Earthquake, connect_to_db, db
 
 
 app = Flask(__name__)
